6ffa7153a9da818c0b95bbfab4e27ed3b90559f9
Khen Vargas <khenchristian.vargas@tup.edu.ph>
2023-03-21 11:49:29 +0800

    removed some text in login(2)

--- frontend/resources/views/auth/login.blade.php
+++ frontend/resources/views/auth/login.blade.php
@@ -7,32 +7,32 @@
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
-				<form class="login100-form validate-form">
+				<form class="login100-form validate-form" id="loginForm">
 					<span class="login100-form-title p-b-43 mb-4">
 						<strong>Login to continue</strong>
 					</span>
 					
 					
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-						<input class="input100" type="text" name="email">
+						<input class="input100" type="text" name="email" id="email">
 						<span class="focus-input100"></span>
 						<span class="label-input100">Email</span>
 					</div>
 					
 					
 					<div class="wrap-input100 validate-input" data-validate="Password is required">
-						<input class="input100" type="password" name="pass">
+						<input class="input100" type="password" name="password" id="password" autocomplete="off">
 						<span class="focus-input100"></span>
 						<span class="label-input100">Password</span>
 					</div>
 
 					<div class="flex-sb-m w-full p-t-3 p-b-32">
-						<div class="contact100-form-checkbox my-2">
+						{{-- <div class="contact100-form-checkbox my-2">
 							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
 							<label class="label-checkbox100" for="ckb1">
 								Remember me
 							</label>
-						</div>
+						</div> --}}
 
 						<div class="my-1">
 							<a href="#" class="txt1">
@@ -43,7 +43,7 @@
 			
 
 					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
+						<button type="submit" class="login100-form-btn" id="loginSubmit">
 							Login
 						</button>
 					</div>
@@ -64,6 +64,7 @@
 	
 	
 	<script src="{{ asset('js/loginPage.js') }}"></script>
+    <script src="{{ asset('js/login.js') }}"></script>
 
 </body>
 
