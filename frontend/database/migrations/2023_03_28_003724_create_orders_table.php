bf1fc8355d91adb8c1537ab5d7989c34440a9c7d
Khen Vargas <khenchristian.vargas@tup.edu.ph>
2023-03-28 08:51:11 +0800

    added order table migrations

---
+++ frontend/database/migrations/2023_03_28_003724_create_orders_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orderinfos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('customer_id')->unsigned();
+            $table->foreign('customer_id')->references('id')->on('customers')
+                ->onDelete('cascade');
+            $table->double('total_purchase');
+            $table->text('status')->default("Order Placed");
+            $table->timestamps();
+        });
+        Schema::create('orderlines', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('orderinfo_id')->unsigned();
+            $table->foreign('orderinfo_id')->references('id')->on('orderinfos')
+                ->onDelete('cascade');
+            $table->integer('product_id')->unsigned();
+            $table->foreign('product_id')->references('id')->on('products')
+                ->onDelete('cascade');
+            $table->integer('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orderinfos');
+        Schema::dropIfExists('orderlines');
+    }
+};
